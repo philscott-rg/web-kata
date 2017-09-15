36867c4a4fdf0bd9350cd506c38510d7c68ab25c
philip.scott <philip.scott@red-gate.com>
2017-09-15 14:38:03 +0100

    kata 1 - with @fffej

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
+import products from './data.js'
+
+var prods = products.products;
 
 class App extends Component {
   render() {
@@ -7,12 +10,30 @@ class App extends Component {
       <div className="App-header">
         <h2>Welcome Introduction to <code>web-kata#1</code></h2>
       </div>
-      <p className="App-intro">
-        To get started change this text and save save to reload.
-      </p>
       <div className='products'>
-        ... your code here
+        <Products data={prods}/>
       </div>
+    </div>
+  }
+}
+
+class Products extends Component {
+  render() {
+    const prodList = prods.map((product) =>
+      <Product key={product.name} name={product.name} description={product.description} free={product.free} new={product.new} />
+    );
+
+    return <div className="products">
+      {prodList}
+    </div> 
+  }
+}
+
+class Product extends Component {
+  render() {
+    return <div className="product" data-free={this.props.free} data-new={this.props.new}>
+      <h1>{this.props.name}</h1>
+      <p>{this.props.description}</p>
     </div>
   }
 }
