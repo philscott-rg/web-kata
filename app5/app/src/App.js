d0ab211c34edf9f7f0c2e7f51171ce502b08c7b5
philip.scott <philip.scott@red-gate.com>
2018-03-28 09:58:21 +0100

    app 5

--- app5/app/src/App.js
+++ app5/app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import data from './data.js'
 
 import ProductMenu from './ProductMenu.js'
 import ProductContainer from './ProductContainer.js'
@@ -9,8 +8,57 @@ import './App.css'
 class App extends Component {
 
   constructor(props) {
-    super(props)
-    this.state = { products: data.products }
+    super(props);
+    this.state = { products: [] };
+    this.getProducts();
+
+    this.remove = this.remove.bind(this);
+    this.handleAddProduct = this.handleAddProduct.bind(this);
+  };
+
+  remove(productName) {
+    fetch(`/api/products/delete/${productName}`, {
+      method: "DELETE"
+    }).then(response => {
+      response.json().then( responseData => {
+        this.setState( { products: responseData } );
+      });
+    }, error => {
+      console.log(error.message);
+    });
+  }
+
+  getProducts( callback ) {
+    fetch('/api/products/get', {
+      method: "GET"
+    }).then(response => {
+      response.json().then( responseData => {
+        this.setState( { products: responseData } );
+      });
+    }, error => {
+      console.log(error.message);
+    });
+  };
+
+  handleAddProduct(event){
+    event.preventDefault()
+
+    fetch('/api/products/add/', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify({
+        "name": event.target.name.value,
+        "description": event.target.description.value
+      })
+    }).then(response => {
+      response.json().then( responseData => {
+        this.setState( { products: responseData } );
+      });
+    }, error => {
+      console.log(error.message);
+    });
   }
 
   render() {
@@ -18,15 +66,25 @@ class App extends Component {
       <div className="App-header">
         <h2>Kata 5 - Interaction with backend server through REST API calls</h2>
       </div>
-      <div className='products-add-product'>add product here</div>
       <div className='products-container'>
-        <ProductMenu products={this.state.products} />
+        <div className='add-product'>
+          <form onSubmit={this.handleAddProduct}>
+            <label>product name:
+              <input type='text' name='name' />
+            </label>
+            <label>description:
+              <input type='text' name='description'/>
+            </label>
+            <input type='submit' value='add product' />
+          </form>
+        </div>
+        <ProductMenu products={this.state.products} remove={this.remove}/>
         <Route exact path='/products/:productName' component={
           props => <ProductContainer {...props} products={this.state.products} />
         } />
       </div>
     </div>
-  }
+  };
 }
 
 export default App
--- app5/app/src/ProductMenu.js
+++ app5/app/src/ProductMenu.js
@@ -7,6 +7,11 @@ class ProductItem extends Component {
     const name = this.props.product.name
     return <div className='product-item'>
       <div className='name'><Link to={'/products/' + name}>{name}</Link></div>
+      <div className="actions">
+        <div className="remove">
+          <span onClick={() => {this.props.remove(this.props.product.name)}}>X</span>
+        </div>
+      </div>
     </div>
   }
 }
@@ -15,7 +20,7 @@ class ProductMenu extends Component {
   render() {
     return <div className='product-menu'>
       {this.props.products.map(
-        (p, i) => <ProductItem product={p} key={'product-' + i} />
+        (p, i) => <ProductItem product={p} key={'product-' + i} remove={this.props.remove} />
       )}
     </div>
   }
